f1cf0aeaa02ebb9f1787555af24e4bccab5764e4
Dominic M Brause <dom.brause@gmail.com>
2019-06-14 04:12:04 +0100

    +complete

--- server.mjs
+++ server.mjs
@@ -1,22 +1,22 @@
 import request from 'request';
 import { readFileSync } from 'fs';
-import { apikey } from './config.json';
+import * as config from './config.json';
 
 const filename = './package.json';
 const content = readFileSync(filename);
-options = {
-            method: "POST",
-            url: 'https://content.dropboxapi.com/2/files/upload',
-            headers: {
-              "Content-Type": "application/octet-stream",
-              "Authorization": "Bearer " + apikey,
-              "Dropbox-API-Arg": "{\"path\": \"/njera/"+filename+"\",\"mode\": \"overwrite\",\"autorename\": true,\"mute\": false}",
-            },
-            body:content
+const options = {
+  method: "POST",
+  url: 'https://content.dropboxapi.com/2/files/upload',
+  headers: {
+    "Content-Type": "application/octet-stream",
+    "Authorization": `Bearer ${config.default.apikey}`,
+    "Dropbox-API-Arg": `{"path": "${filename}", "mode": "overwrite", "autorename": true, "mute": false}`,
+  },
+  body: content
 };
 
- request(options,function(err, res,body){
-     console.log("Err : " + err);
-     console.log("res : " + res);
-     console.log("body : " + body);    
+request(options, (error, res, body) => {
+  console.log(`Error: ${error}`);
+  console.log(`Response: ${JSON.stringify(res)}`);
+  console.log(`Body: ${body}`);
 });
